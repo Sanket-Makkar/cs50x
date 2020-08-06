5c3cd81e372f3dcc274ce4802fbc1834753f293a
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-06 18:38:29 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,32 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <math.h>
-    int c = 0;
 int main(void){
-    printf("change in dollars: ");
-    float x;
-    if (x >= 0){
-        scanf("%f", &x);
-        while (x >= .25){
-            c = c + 1;
-            x = x - .25;
-
-        }
-        while (x >= .1){
-            c = c + 1;
-            x = x - .1;
-            
-        }
-        while (x >= .05){
-            c = c + 1;
-            x = x - .05;
-            
-        }
-        while (x >= .01){
-            c = c + 1;
-            x = x - .01;
-            
-        }
-        printf("%d", c);
+    printf("Change: ");
+    int c = 0;
+    int a;
+    scanf("%d", &a);
+    
+    if (a <= .25){
+        c = c + 1;
+        a = a - .25
     }
 }
