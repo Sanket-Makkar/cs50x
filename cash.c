288781d624f6f0f51494dd7aea92ee7900448a93
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-07 18:35:56 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -1,13 +1,31 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
+
 int main(void){
     printf("Change: ");
+    float x; 
     int c = 0;
-    int a;
-    scanf("%d", &a);
+    scanf("%f", &x);
     
-    if (a <= .25){
-        c = c + 1;
-        a = a - .25
+    while (x != 0){
+        if (x >= .25){
+            x = x - .25;
+            c++;
+        }
+        else if (x >= .1){
+            x = x - .1;
+            c++;
+        }
+        else if (x >= .05){
+            x = x - .05;
+            c++;
+        }
+        else if (x >= .01){
+            x = x - .01;
+            c++;
+        }
+    }
+    if (x == 0){
+        printf("%d", c);
     }
 }
