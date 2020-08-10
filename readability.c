99e2d458c4ffe6db2bb8afec6ff8bd763cf0f48c
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-10 14:45:00 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -8,25 +8,24 @@
 // you need
 
 int main(void){
-    //letter count
+    //letters
     int L = 0;
     //word count
     int S = 1;
     //sentence count
     int U = 0;
-    // space count
-    int o = 0;
 
     //get lines
     string lines = get_string("Text: ");
 
     for(int a = 0; a < strlen(lines); a++){
         //count letters
-        L = L + 1;
-        //count words and spaces
+        if ((lines[a] >= 'a' && lines[a] <= 'z') || (lines[a] <= 'Z' && lines[a] >= 'A')){
+            L = L + 1;
+        }
+        //count words 
         if (lines[a] == ' '){
             S++;
-            o++;
         }
         //count sentences
         if (lines[a] == '.' || lines[a] == '?' || lines[a] == '!'){
@@ -34,20 +33,20 @@ int main(void){
         }
     }
 
-   // letter total
-    int b = L - o;
 
-   // letters per 100 words
-   int g = ((float)b/(float)S)*100;
-
-   // sentences per 100 words
-   int h = ((float)U/(float)S)*100;
 
 //final calculation
-   int i = .0588 * g - .296 * h - 15.8;
+   int i =  0.0588 * (100 * (float) L / (float) S) - 0.296 * (100 * (float) U / (float) S) - 15.8;
 
-
+if((i < 16) && (i > 0)){
     printf("Grade %d", i);
-  // L is for avg letters per 100 words
-  // S is for avg sentences per 100 letters
+}
+else if (i > 16){
+    printf("grade 16+");
+}
+else if (i < 1){
+    printf("before grade 1");
+}
+
+
 }
