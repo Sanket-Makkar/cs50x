8c5ad0952d318a9f5397fc39e5a88c555fd25a19
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-11 20:25:16 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -8,14 +8,18 @@
 int main(int argc, string argv[]){
     
     for(int p = 0; p < strlen(argv[1]); p++ ){
-        if (!isdigit(*argv[1])){
-            printf("please try and add another value after the caesar command\n");
+        if(isdigit(argv[1]) != 0){
+            printf("try putting a number instead...\n");
             return 1;
         }
     }
 
     if (argc != 2){
         printf("try again with a number after caesar...\n");
+        return 1;
+    }
+    if (!isdigit(*argv[1])){
+        printf("please try and add another value after the caesar command\n");
         return 1;
     }
     if (isalpha(*argv[1])){
