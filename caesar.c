934df331718828ce6e476b253756f45e1b686bcb
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-11 20:29:27 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -6,29 +6,24 @@
 #include <stdlib.h>
 // whats left to do: adjust for capital letters, and loop z to a
 int main(int argc, string argv[]){
-    
-    for(int p = 0; p < strlen(argv[1]); p++ ){
-        if(isdigit(argv[1]) != 0){
-            printf("try putting a number instead...\n");
-            return 1;
-        }
-    }
-
+    int n;
+    n = atoi (argv[1]);
     if (argc != 2){
         printf("try again with a number after caesar...\n");
         return 1;
     }
+    for(int p = 0; p < strlen(argv[1]); p++){
     if (!isdigit(*argv[1])){
         printf("please try and add another value after the caesar command\n");
         return 1;
+    }
     }
     if (isalpha(*argv[1])){
         printf("try putting a number instead...\n");
         return 1;
     }
 
-    int n;
-    n = atoi (argv[1]);
+
 
     string line = get_string("text: ");
 
