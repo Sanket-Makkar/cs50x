fef0fb9fec79933bf70d788672d2560b0698ff92
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-11 17:41:24 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -13,7 +13,7 @@ string line = get_string("text: ");
         printf("Usage is: ./caesar k\n");
         return 1;
     }
-    if (isdigit(*argv[1])){
+    if (isdigit(*argv[1]) != 0){
     int n;
     n = atoi (argv[1]);
     if ((n >= 0) && (n <= 10000)){
