3910d46f170e87477265e42a83fdd17c62068c65
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-13 16:25:51 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -8,17 +8,17 @@ int main(void)
     {
         x = get_int("height: ");
     }
-    while(x <= 0 || x > 8);
-            for (int y = 0; y < x; y = y + 1)
-            {
-                for (int k = 0; k < x - y - 1; k = k + 1)
-                {
-                    printf(" ");
-                }
-                for (int j = 0; j <= y; j = j + 1)
-                {
-                    printf("#");
-                }
-                printf("\n");
-            }
+    while (x <= 0 || x > 8);
+    for (int y = 0; y < x; y = y + 1)
+    {
+        for (int k = 0; k < x - y - 1; k = k + 1)
+        {
+            printf(" ");
         }
+        for (int j = 0; j <= y; j = j + 1)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
