147189fde33c3aba0774f6f13324d22ae0a67d37
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-13 15:55:21 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 int main(void)
 {
     printf("height: ");
@@ -24,35 +25,10 @@ int main(void)
                     printf("\n");
                 }
             }
-            else
-            {
-                printf("height: ");
-                int x;
-                scanf("%d", &x);
-                if (x <= 8)
-                {
-                    if (x >= 1)
-                    {
-                        for (int y = 0; y < x; y = y + 1)
-                        {
-                            for (int k = 0; k < x - y - 1; k = k + 1)
-                            {
-                                printf(" ");
-                            }
-                            for (int j = 0; j <= y; j = j + 1)
-                            {
-                                printf("#");
-                            }
-                            printf("\n");
-                        }
-                    }
-                }
-            }
         }
     }
-}
-else
-{
-    printf("try again with a value...");
-}
+    else
+    {
+        printf("try again with a value...");
+    }
 }
