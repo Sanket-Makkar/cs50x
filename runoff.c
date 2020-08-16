0e35f76e013741d7d5a2e253f3c1552596dc3115
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-16 16:39:48 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -142,16 +142,19 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < voter_count; i++)
+    
+    for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; i < voter_count; i++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if(!candidates[i].eliminated)
             {
                 candidates[preferences[i][j]].votes++;
             }
         }
-    }
+    }    
+    
+
     return;
 }
 
@@ -172,26 +175,27 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int minimumvotes = 100;
+    int min = 100;
     for (int i = 0; i < candidate_count; i++)
     {
-        if(!candidates[i].eliminated)
+        if (candidates[i].votes < min)
         {
-            minimumvotes = candidates[i].votes;
+            min = candidates[i].votes;
         }
     }
     return 0;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
-bool is_tie(int minimumvotes)
+bool is_tie(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (!candidates[i].eliminated){
-            if(candidates[i].votes != minimumvotes)
+        if (!candidates[i].eliminated)
+        {
+            if (candidates[i].votes == min)
             {
-                return false;
+                return true;
             }
         }
     }
@@ -199,18 +203,14 @@ bool is_tie(int minimumvotes)
 }
 
 // Eliminate the candidate (or candidates) in last place
-void eliminate(int minimumvotes)
+void eliminate(int min)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if(!candidates[i].eliminated)
+        if (candidates[i].votes == min)
         {
-            if (candidates[i].votes == minimumvotes)
-            {
-                candidates[i].eliminated = true;
-            }
+            candidates[i].eliminated = true;
         }
     }
     return;
 }
-
