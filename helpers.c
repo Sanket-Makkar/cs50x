7d207323cf7a2b15e1d7b62aef2c4452913e1ce5
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-18 20:39:12 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -32,16 +32,19 @@ int ensurewithin(int value)
 }
 
 // Convert image to sepia
-int prevent_maximum_limit(int value);
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int h = 0; h < height; h++)
     {
         for (int w = 0; w < width; w++)
         {
-            image[h][w].rgbtRed = ensurewithin(round(0.393 * image[h][w].rgbtRed + 0.769 * image[h][w].rgbtGreen + 0.189 * image[h][w].rgbtBlue));
-            image[h][w].rgbtGreen = ensurewithin(round(0.349 * image[h][w].rgbtRed + 0.686 * image[h][w].rgbtGreen + 0.168 * image[h][w].rgbtBlue));
-            image[h][w].rgbtBlue = ensurewithin(round(0.272 * image[h][w].rgbtRed + 0.534 * image[h][w].rgbtGreen + 0.131 * image[h][w].rgbtBlue));
+            int sepiaRed = ensurewithin(round(0.393 * image[h][w].rgbtRed + 0.769 * image[h][w].rgbtGreen + 0.189 * image[h][w].rgbtBlue));
+            int sepiaGreen = ensurewithin(round(0.349 * image[h][w].rgbtRed + 0.686 * image[h][w].rgbtGreen + 0.168 * image[h][w].rgbtBlue));
+            int sepiaBlue = ensurewithin(round(0.272 * image[h][w].rgbtRed + 0.534 * image[h][w].rgbtGreen + 0.131 * image[h][w].rgbtBlue));
+            
+            image[h][w].rgbtRed = sepiaRed;
+            image[h][w].rgbtGreen = sepiaGreen;
+            image[h][w].rgbtBlue = sepiaBlue;
         }
     }
     return;
