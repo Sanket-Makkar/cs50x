cbe2c85fed78699832eab1c4c32942b2bdb8a129
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-18 16:26:18 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -3,15 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int h = 0; h < height - 1; h++)
+    for (int h = 0; h < height; h++)
     {
-        for (int w = 0; w < width - 1; w++)
+        for (int w = 0; w < width; w++)
         {
-            float avgpxl = round((image[h][w].rgbtRed + image[h][w].rgbtBlue + image[h][w].rgbtGreen)/3);
-            image[h][w].rgbtRed = avgpxl;
-            image[h][w].rgbtGreen = avgpxl;
-            image[h][w].rgbtBlue = avgpxl;
-            
+            image[h][w].rgbtRed = image[h][w].rgbtRed + 0;
+            image[h][w].rgbtBlue = image[h][w].rgbtRed;
+            image[h][w].rgbtGreen = image[h][w].rgbtRed; 
         }
     }
     return;
@@ -21,60 +19,13 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 int prevent_maximum_limit(int value);
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    for (int h = 0; h < height - 1; h++)
-    {
-        for (int w = 0; w < width - 1; w++)
-        {
-            RGBTRIPLE holder = image[h][w];
-            int R = holder.rgbtRed;
-            int G = holder.rgbtGreen;
-            int B = holder.rgbtBlue;
-            image[h][w].rgbtRed = prevent_maximum_limit(round(.393 * R + .769 * G + .189 * B));
-            image[h][w].rgbtGreen = prevent_maximum_limit(round(.349 * R + .686 * G + .168 * B));
-            image[h][w].rgbtBlue = prevent_maximum_limit(round(.272 * R + .534 * G + .131 * B));
-        }
-    }
     return;
 }
-
-int prevent_maximum_limit(int value)
-{
-    if (value > 255)
-    {
-        value = 255;
-    }
-    return value;
-}
-
-
-
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int filler_var[3];
-    for (int h = 0; h < height - 1; h++)
-    {
-        for (int w = 0; w < width/2; w++)
-        {
-            filler_var[0] = image[h][w].rgbtRed;
-            filler_var[1] = image[h][w].rgbtGreen;
-            filler_var[2] = image[h][w].rgbtBlue;
-            
-            image[h][w].rgbtRed = image[h][width - 1 - w].rgbtRed;
-            image[h][w].rgbtGreen = image[h][width - 1 - w].rgbtGreen;
-            image[h][w].rgbtBlue = image[h][width - 1 - w].rgbtBlue;
-            
-            image[h][width - 1 - w].rgbtRed = filler_var[0];
-            image[h][width - 1 - w].rgbtGreen = filler_var[1];
-            image[h][width - 1 - w].rgbtBlue = filler_var[2];
-            
-// filler = img1
-// img1 = img0
-// img0 = filler
-        }
-        return;
-    }
+    return;
 }
 
 // Blur image
