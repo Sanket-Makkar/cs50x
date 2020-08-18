c2bac2399c9ebaedb585793bcdc770dc7b9e3a8b
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-08-18 15:09:30 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int w = 0; w < width - 1; w++)
         {
             RGBTRIPLE holder = image[h][w];
-            int avgpxl = (round(holder.rgbtRed + holder.rgbtBlue + holder.rgbtGreen)/3);
+            int avgpxl = (round(image[h][w].rgbtRed + image[h][w].rgbtBlue + image[h][w].rgbtGreen)/3);
             image[h][w].rgbtRed = avgpxl;
             image[h][w].rgbtGreen = avgpxl;
             image[h][w].rgbtBlue = avgpxl;
