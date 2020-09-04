9b572685b5fe664b64a3e17fd04e6a099d7dc47f
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-09-04 15:36:22 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -11,13 +11,13 @@ int main(int argc, char *argv[])
         printf("Invalid usage. Usage: ./recover image\n");
         return 1;
     }
-    FILE * filedata = fopen("card.raw", "rb");
+    FILE *filedata = fopen("card.raw", "rb");
     BYTE buffer[512];
     bool jpgfound = false;
     int filecount = 0;
     char *filename = NULL;
-    filename = (char*)malloc(8*sizeof(char));
-    FILE * unrecoveredpics = NULL;
+    filename = (char *)malloc(8 * sizeof(char));
+    FILE *unrecoveredpics = NULL;
     do
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xe0) == 0xe0)
@@ -27,24 +27,24 @@ int main(int argc, char *argv[])
                 printf("format is ./recover card.raw");
                 return 1;
             }
-                if (jpgfound == true)
-                {
-                    fclose(unrecoveredpics);
-                }
-                else if (jpgfound == false)
-                {
-                    jpgfound = true;
-                }
-                sprintf(filename, "%03i.jpg", filecount);
-                filecount++;
-                unrecoveredpics = fopen(filename, "wb");
-                
+            if (jpgfound == true)
+            {
+                fclose(unrecoveredpics);
+            }
+            else if (jpgfound == false)
+            {
+                jpgfound = true;
+            }
+            sprintf(filename, "%03i.jpg", filecount);
+            filecount++;
+            unrecoveredpics = fopen(filename, "wb");    
         }
         if (filecount > 0)
         {
             fwrite(&buffer, 512, 1, unrecoveredpics);
         }
-    }while(fread(buffer, 512, 1, filedata));
+    }
+    while (fread(buffer, 512, 1, filedata));
     fclose(filedata);
     fclose(unrecoveredpics);
 }
