0ca6e3630b7f82401971667f906cf00a94967c29
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-09-19 02:55:55 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
-int main(){
-char name[100];
-printf("Enter your name: \n");
-scanf("%s", name);
-printf("Hello %s\n", name);
+int main()
+{
+    char name[100];
+    printf("Enter your name: \n");
+    scanf("%s", name);
+    printf("Hello %s\n", name);
 }
