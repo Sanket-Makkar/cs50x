95319a7013a90bebd5222d3aa9fe1955ad8b1cf0
sanketmakkar <sanketmakkar@users.noreply.github.com>
2020-11-12 05:59:31 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -6,7 +6,7 @@ import csv
 if (len(sys.argv) != 2):
     sys.exit("Usage: python roster.py housename")
 db = SQL("sqlite:///students.db")
-if ((sys.argv[1] not in "Gryffindor") & (sys.argv[1] not in "Slytherin") & (sys.argv[1] not in "Ravenclaw") % (sys.argv[1] not in "Hufflepuff")):
+if ((sys.argv[1] not in "Gryffindor") & (sys.argv[1] not in "Slytherin") & (sys.argv[1] not in "Ravenclaw") & (sys.argv[1] not in "Hufflepuff")):
     sys.exit("Usage: python roster.py housename")
 i = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", sys.argv[1])
 for k in i:
